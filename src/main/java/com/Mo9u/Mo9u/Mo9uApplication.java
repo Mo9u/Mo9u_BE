d3eb8162dc596fddd63e1e182c22038063bf9dbc
kjyyjk <rlawnsdud920@gmail.com>
2023-08-03 01:47:37 +0900

    Initial commit

---
+++ src/main/java/com/Mo9u/Mo9u/Mo9uApplication.java
@@ -0,0 +1,13 @@
+package com.Mo9u.Mo9u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mo9uApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mo9uApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Mo9u/Mo9u/Mo9uApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mo9u.Mo9u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mo9uApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
