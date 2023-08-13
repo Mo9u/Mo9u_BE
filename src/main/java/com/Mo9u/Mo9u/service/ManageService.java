7443e4485c3be11b4b2a81bdd7045caa004ffe55
hhbb0081 <likesun2000@naver.com>
2023-08-14 00:26:24 +0900

    Docs: annotation 추가

--- src/main/java/com/Mo9u/Mo9u/service/ManageService.java
+++ src/main/java/com/Mo9u/Mo9u/service/ManageService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+@Service
 @RequiredArgsConstructor
 public class ManageService {
 
